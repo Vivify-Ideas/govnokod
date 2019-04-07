35b2a865a6e20af5744feb91a1f980748e43f715
Nikola Spalevic <nikola.s@vivifyideas.com>
2019-04-07 10:21:34 +0200

    Added Readme

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>💩GovnoKod💩</h1>
+
+## Description
+
+Shit and Code in one place. 💩
+Shit happens
+
+Graphql implementation
+
+[Live Demo](https://govnokod.fun)
+
+## Stack
+- NestJS (Graphql: ApolloServer)
+- Vue (VueApollo)
--- govnokod-fe/src/vue-apollo.js
+++ govnokod-fe/src/vue-apollo.js
@@ -8,7 +8,7 @@ import { AUTH_TOKEN } from './constants'
 Vue.use(VueApollo)
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://localhost:3000/graphql'
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://api.govnokod.fun/graphql'
 // Files URL root
 export const filesRoot = process.env.VUE_APP_FILES_ROOT || httpEndpoint.substr(0, httpEndpoint.indexOf('/graphql'))
 
